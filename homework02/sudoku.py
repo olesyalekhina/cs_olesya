8d13d50a9189cd4dc7519d11e1194536833e8a7a
Алехина Олеся Сергеевна <71012628+olesyalekhina@users.noreply.github.com>
2020-11-14 14:43:40 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -146,13 +146,23 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    position = find_empty_positions(grid)
+    if not position:
+        return grid
+    row,column=position
+    for i in find_possible_values(grid, position):
+        grid[row][column] = i
+        result = solve(grid)
+        if result:
+            return result
+    grid[row][column] = "."
+    return None
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """ Если решение solution верно, то вернуть True, в противном случае False """
     # TODO: Add doctests with bad puzzles
-    p
+    pass
 
 
 def generate_sudoku(N: int) -> tp.List[tp.List[str]]:
